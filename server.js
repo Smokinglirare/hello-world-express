bedd40311065bd0b288d74d24a67108317248a4f
Sebastian <sebastian.thorneus@gmail.com>
2022-08-15 10:40:59 +0100

    .env

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const PORT = process.env.PORT || 4000;
+const dummy = DUMMY_TEXT;
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World! hahahaha nejnejnejnej");
+  res.send(`${dummy}`);
 });
 
 app.listen(PORT, () => {
